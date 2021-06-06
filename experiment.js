77a06a018f68d0559fa51d44c25c807d6a91d6a7
Chhavi <Chhavi.choudhary75@gmail.com>
2021-06-06 21:45:11 +0200

    inspect

--- experiment.js
+++ experiment.js
@@ -1,3 +1,12 @@
 const monthlyRent=500;
 const YearlyRent=monthlyRent*12;
-console.log(YearlyRent);
+console.log(YearlyRent,monthlyRent);
+yearlyRent=200;
+console.log(yearlyRent,monthlyRent);
+const firstName="chhavi";
+const lastName="choudhary";
+const sentence=`Hello ${firstName} ${lastName}`;
+console.log(sentence);
+
+const istheSkyBlue=true;
+console.log(istheSkyBlue);
