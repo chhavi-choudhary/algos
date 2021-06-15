63294786d817cff4109b35b7c73f2dbc0cff853d
Chhavi <Chhavi.choudhary75@gmail.com>
2021-06-15 20:13:13 +0200

    looping

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Add js to html
+# learn how to add javascript file to js(index.html)
+# loops-Write some code that declares two variables, character and timesToRepeact. Using a loop, repeat that character that many times and then console.log it.Example, if I had character = 'f' and timesToRepeat = 5, it'd console.log'fffff'.
---
+++ loop.js
@@ -0,0 +1,26 @@
+// let friendsAtYourParty=0;
+// // while(friendsAtYourParty<10){
+// //     friendsAtYourParty=friendsAtYourParty+1;
+// //     friendsAtYourParty++;
+// //     ++friendsAtYourParty;
+// //     friendsAtYourParty+=1;
+// //     console.log(friendsAtYourParty);
+// // }
+// // console.log(friendsAtYourParty);
+
+
+
+// for (let index = 0; index <10; index++) {
+//     friendsAtYourParty++;
+//     console.log(friendsAtYourParty);
+// }
+// console.log(friendsAtYourParty);
+
+const character='f';
+const timeToRepeat=10;
+let myString='';
+for(let i=0;i<=timeToRepeat;i++){
+  myString= myString.concat(character);
+}
+
+console.log(myString);
