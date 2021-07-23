951b5789c0a98a70a39f26192e94846ced7cd447
Chhavi <Chhavi.choudhary75@gmail.com>
2021-07-23 16:37:55 +0200

    add arrays

---
+++ array.js
@@ -0,0 +1,6 @@
+const primeNumbers = [1, 2, 3, 5, 7, 11, 13, 17];
+
+console.log(primeNumbers.length);
+console.log(primeNumbers.join("|"));
+console.log(primeNumbers.push(18,19));
+console.log(primeNumbers)
