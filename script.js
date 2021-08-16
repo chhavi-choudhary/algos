71293f1404aff3df11294c2e25876677ddc0bdbc
Chhavi <Chhavi.choudhary75@gmail.com>
2021-08-16 23:58:49 +0200

    multiple selectins

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const square=document.querySelector('.redsquare');
 square.style.backgroundColor="blue";
+const square1=document.querySelectorAll('.greensquare');
+square1.style.backgroundColor="pink";
