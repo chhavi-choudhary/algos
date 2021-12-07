d9bc485c4c4f14bcadd47c0181a2589ed4d3c36b
Chhavi <Chhavi.choudhary75@gmail.com>
2021-12-07 22:15:50 +0100

    Dom

---
+++ script2.js
@@ -0,0 +1,5 @@
+let secondElem=document.getElementById("second");
+secondElem.innerText="I am new to this second group";
+let thirdElem=document.getElementById("third");
+thirdElem.innerHTML="I am <em>new team member</em> to this second group";
+secondElem.style.fontWeight="bold"
